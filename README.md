c1ac5ab99520d35f8f48dfd801ca9858ec4280d7
didinkoe <kydidin@gmail.com>
2020-11-15 15:48:05 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# didinkoe.github.io
---
+++ style/script.js
@@ -0,0 +1,13 @@
+// const judul = document.querySelector('#judul');
+// src.setAttribute('class', 'makassarcoding');
+
+const kotak = document.querySelector('#kotak');
+const btn = document.querySelector('#btn');
+btn.onclick = ubahMerah;
+function ubahMerah() {
+    kotak.style.backgroundColor = 'red';
+}
+btn1.onclick = ubahBiru
+function ubahBiru() {
+    kotak.style.backgroundColor = 'blue';
+}
